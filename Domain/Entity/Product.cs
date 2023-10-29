c90e69ae82fc966a89301fbac261a20ad1055d70
SnappFood <SnappFood@SF-0353>
2023-10-29 13:04:49 +0330

    Add project files.

---
+++ Domain/Entity/Product.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entity
+{
+	public class Product:BaseEntity
+	{
+		public string Name { get; set; }
+		public string BarCode { get; set; }
+		public string Description { get; set; }
+		public string Rate {  get; set; }
+
+	}
+}
